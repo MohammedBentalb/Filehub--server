58cc4d31d9279fa0a74a76d89e3a291fed2778b0
mohammed bentalb <mohammedbentalb2000@gmail.com>
2024-03-09 19:03:50 +0100

    changing upload's route structure

--- routes/data.js
+++ routes/data.js
@@ -6,11 +6,15 @@ const insertFileHandler = require('../controller/data/insertFileHandler');
 const paginationOptions = require('../controller/pagination.js/paginationOptions');
 const asyncWrapper = require('../middleware/asyncWrapper');
 
+const multer = require('multer')
+
+const upload = multer({dest: 'uploads'})
+
 route.get('/files', asyncWrapper(paginationOptions), asyncWrapper(getAllFilesHandler)); // route to get all files
-route.post('/files/:fileId', asyncWrapper(insertFileHandler)); // route to insert a file
-route.get('/files/:fileId', asyncWrapper(getSingleFileHandler)); // route to get a file
+route.get('/files/:fileId', asyncWrapper(getSingleFileHandler)); // route to get a Single file
 route.get('/author/:authorId', asyncWrapper(getAuthorFilesAndInfoHandler));  // route to get the user's info and files
 route.delete('/files/:fileId', asyncWrapper(() => {})); // route to delete a file
 route.get('/files/download/:fileId', asyncWrapper(() => {})); // route to download a file
+route.post('/files/upload/',upload.single('file'), asyncWrapper(insertFileHandler)); // route to insert a file
 
 module.exports = route;
