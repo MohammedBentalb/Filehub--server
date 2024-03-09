0ac34d5952fde275f10a58f29f2eba728d78f6e8
mohammed bentalb <mohammedbentalb2000@gmail.com>
2024-03-09 14:28:14 +0100

    verifying with token  instead of cookie 🍪

--- controller/routeProtection/verificationHandler.js
+++ controller/routeProtection/verificationHandler.js
@@ -3,6 +3,9 @@ const jwt = require('jsonwebtoken');
 const { createCustomError } = require('../../lib/customError');
 const User = require('../../models/Users');
 
+/*
+verifying with cookie in devMode with postMan only for speeding purposes only (not to be user in production)
+
 const verificationHandler = async (req, res, next) => {
   const refreshToken = req.cookies.jwt;
   if (!refreshToken) return next(createCustomError('UnAuthorized', 401));
@@ -15,5 +18,27 @@ const verificationHandler = async (req, res, next) => {
     res.author = foundUser;
     next();
   });
+}; */
+
+const verificationHandler = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) return next(createCustomError('UnAuthorized', 401));
+
+  const token = authHeader.split(' ')[1]
+   jwt.verify(
+    token,
+    process.env.SECRET_TOKEN,
+    async (err, decoded) => {
+      if (err) return next(createCustomError('UnAuthorized', 401));
+      const foundUser = await User.findOne({ email: decoded.email });
+      if ( !foundUser ||
+        Object.keys(foundUser).length === 0 ||
+        foundUser.email !== decoded.email
+      )
+        return next(createCustomError('UnAuthorized User', 401));
+      res.author = foundUser;
+      next();
+    }
+  ); 
 };
 module.exports = verificationHandler;
